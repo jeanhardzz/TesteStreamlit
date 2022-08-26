91ae2d71f8fe35c3b433b1631effb3c0d13cb3dc
Fulano de Tal <jean.lk@hotmail.com>
2022-08-26 19:42:27 -0300

    a

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ import streamlit as st
 
 st.title("Opa Tudo Bom!")
 
-st.selectbox("Selecione um bro",["pablin","bias","felope"])
+st.selectbox("Selecione um bro",["pablin","bias","felope","pedron","jubplay","vitao"])
 
 st.sidebar.title("Cuidado")
