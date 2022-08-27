c0192396a5a9ef6266e528bf79e9e10c9fa7fceb
Fulano de Tal <jean.lk@hotmail.com>
2022-08-27 01:18:45 -0300

    barra

--- main.py
+++ main.py
@@ -5,3 +5,13 @@ st.title("Opa Tudo Bom!")
 st.selectbox("Selecione um bro",["pablin","bias","felope","pedron","jubplay","vitao"])
 
 st.sidebar.title("Cuidado")
+
+options = {
+    "xAxis": {
+        "type": "category",
+        "data": ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+    },
+    "yAxis": {"type": "value"},
+    "series": [{"data": [120, 200, 150, 80, 70, 110, 130], "type": "bar"}],
+}
+st_echarts(options=options, height="500px")
